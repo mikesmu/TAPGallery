b4c24983aa48dfa429226f35467d0d4816c79080
Michał Smulski <smulski.michal@gmail.com>
2012-12-13 15:56:13 +0100

    Initial Commit

---
+++ TAPGallery/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TAPGallery
+//
+//  Created by Michał Smulski on 13.12.2012.
+//  Copyright (c) 2012 TAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TAPGallery/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TAPGallery
+//
+//  Created by Michał Smulski on 13.12.2012.
+//  Copyright (c) 2012 TAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
